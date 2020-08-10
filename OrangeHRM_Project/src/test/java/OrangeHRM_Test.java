9b4dfe2bc00a39d01fd842c9719cf414885eee10
olenapasere <olenapasere@gmail.com>
2020-08-10 19:40:13 -0400

    Lena's commit

--- OrangeHRM_Project/src/test/java/OrangeHRM_Test.java
+++ OrangeHRM_Project/src/test/java/OrangeHRM_Test.java
@@ -1,8 +1,11 @@
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -23,21 +26,16 @@ public class OrangeHRM_Test {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("https://orangehrm-demo-6x.orangehrmlive.com/auth/login");
-        wait = new WebDriverWait(driver, 20);
+        wait = new WebDriverWait(driver, 5);
     }
 
-    @Test
-    public void loginAsA() {
-
-    }
 
 
 
 
 
     @AfterMethod
-    public void tearDown(){
-        driver.quit();
+    public void tearDown(){ driver.quit();
     }
 
 
